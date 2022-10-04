747106a1e69e4ad0111395972ed55adcc0393666
vZidv <94315999+vZidv@users.noreply.github.com>
2022-10-04 17:32:53 +0300

    ver 1
    Add Authorization wind

---
+++ HrHelper/HrHelper/Pages/Authorization_page.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HrHelper.Pages
+{
+    /// <summary>
+    /// Interaction logic for Authorization_page.xaml
+    /// </summary>
+    public partial class Authorization_page : Page
+    {
+        public Authorization_page()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ HrHelper/HrHelper/Windows/Authorization_win.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.IO;
+namespace HrHelper.Windows
+{
+    /// <summary>
+    /// Interaction logic for Authorization_win.xaml
+    /// </summary>
+    public partial class Authorization_win : Window
+    {
+        public Authorization_win()
+        {
+            InitializeComponent();
+
+            mainFrame.Content = new Pages.Authorization_page();
+        }
+
+        private void ToolBar_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+    }
+}
