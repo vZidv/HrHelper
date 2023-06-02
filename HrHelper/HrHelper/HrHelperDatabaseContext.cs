248da03202b1afeece235b48e7f433fa05fde20f
vZidv <94315999+vZidv@users.noreply.github.com>
2023-06-02 09:08:29 +0300

    ver 2.7.5 Fixed json sql string

--- HrHelper/HrHelper/HrHelperDatabaseContext.cs
+++ HrHelper/HrHelper/HrHelperDatabaseContext.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
 
@@ -34,10 +35,10 @@ namespace HrHelper
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer(Classes.Settings.GetFrommJson("ConnectionString"));
+                optionsBuilder.UseSqlServer($"Data Source={Classes.Settings.GetFrommJson("sqlServerName")};AttachDbFilename={Directory.GetCurrentDirectory()}\\Database\\HrHelperDatabase.mdf;Integrated Security=True;Encrypt = false; Connect Timeout=10;");
             }
         }
-
+        //Classes.Settings.GetFrommJson("ConnectionString")
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<AuthorizationUser>(entity =>
